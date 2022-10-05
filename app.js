5e3f3f8730170b3aa0e32722295d4e988352dc23
kur95 <dogga259@gmail.com>
2022-10-05 17:45:00 +0300

    fix version

--- app.js
+++ app.js
@@ -13,3 +13,26 @@ function clearActiveClasses() {
         slide.classList.remove('active')
     })
 }
+
+
+// function slidesPlugin(activeSlide = 0) {
+//     const slides = document.querySelectorAll('.slide')
+
+//     slides[activeSlide].classList.add('active')
+
+//     for (const slide of slides) {
+//         slide.addEventListener('click', () => {
+//             clearActiveClasses()
+
+//             slide.classList.add('active')
+//         })
+//     }
+
+//     function clearActiveClasses() {
+//         slides.forEach((slide) => {
+//             slide.classList.remove('active')
+//         })
+//     }
+// }
+
+// slidesPlugin(4)
